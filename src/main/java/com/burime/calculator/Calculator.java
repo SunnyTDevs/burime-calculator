54ff6468cb1e5fd2f80c36c03df277213491c34e
s.ermoshenko <s.ermoshenko@tbank.ru>
2026-04-05 18:34:39 +0300

    Ввод первого числа

--- src/main/java/com/burime/calculator/Calculator.java
+++ src/main/java/com/burime/calculator/Calculator.java
@@ -6,5 +6,6 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите первое число:");
+        int a = scanner.nextInt();
     }
 }
