f3af2fcabcc736fe3ceaca2eb25c5c4bdf906500
v.suslova <koshovnaa@gmail.com>
2026-04-05 18:38:22 +0300

    Приглашение пользователя ввести второе число

--- src/main/java/com/burime/calculator/Calculator.java
+++ src/main/java/com/burime/calculator/Calculator.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число:");
+        System.out.println("Введите первое число: ");
         int a = scanner.nextInt();
         System.out.println("Введите операцию (+, -):");
         String op = scanner.next();
+        System.out.println("Введите второе число:");
     }
 }
